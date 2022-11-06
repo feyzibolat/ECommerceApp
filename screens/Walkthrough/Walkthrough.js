9afa26811a61c806a409e6fd6c69b734f0ce4d3c
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-11-06 14:39:07 +0300

    Update Walkthrough.js

--- screens/Walkthrough/Walkthrough.js
+++ screens/Walkthrough/Walkthrough.js
@@ -14,6 +14,97 @@ const Walkthrough = () => {
 
     const scrollX = React.useRef(new Animated.Value(0)).current;
 
+    const Dots = () => {
+
+        const dotPosition = Animated.divide(scrollX, SIZES.width)
+
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                {constants.walkthrough.map((item, index) => {
+                    const dotColor = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [COLORS.dark08, COLORS.primary, COLORS.dark08],
+                        extrapolate: 'clamp'
+                    })
+
+                    return (
+                        <Animated.View
+                            key={`dot-${index}`}
+                            style={{
+                                borderRadius: 5,
+                                marginHorizontal: 6,
+                                width: 10,
+                                height: 10,
+                                backgroundColor: dotColor
+                            }}
+                        />
+                    )
+                })}
+            </View>
+        )
+    }
+
+    function renderFooter() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: SIZES.height * 0.2,
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: SIZES.padding,
+                    paddingVertical: SIZES.height > 700 ? SIZES.padding : 20
+                }}
+            >
+                {/* Dots */}
+                <Dots />
+
+                {/* Buttons */}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        height: 55
+                    }}
+                >
+                    <TextButton
+                        label="Join Now"
+                        contentContainerStyle={{
+                            flex: 1,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGrey
+                        }}
+                        labelStyle={{
+                            color: COLORS.primary,
+                            ...FONTS.h3
+                        }}
+                    />
+
+                    <TextButton
+                        label="Log In"
+                        contentContainerStyle={{
+                            flex: 1,
+                            marginLeft: SIZES.radius,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.primary
+                        }}
+                        labelStyle={{
+                            ...FONTS.h3
+                        }}
+                    />
+                </View>
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
@@ -43,14 +134,51 @@ const Walkthrough = () => {
                                 justifyContent: 'center'
                             }}
                         >
-                            <Text>
-                                {item.title}
-                            </Text>
+                            {/* Walkthrough Images */}
+                            <View
+                                style={{
+                                    flex: 1,
+                                }}
+                            >
+
+                            </View>
+
+                            {/* Title & Descriptions */}
+                            <View
+                                style={{
+                                    height: SIZES.height * 0.35,
+                                    alignItems: 'center',
+                                    justifyContent: 'flex-start',
+                                    paddingHorizontal: SIZES.padding
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        ...FONTS.h1
+                                    }}
+                                >
+                                    {item.title}
+                                </Text>
+
+                                <Text
+                                    style={{
+                                        marginTop: SIZES.radius,
+                                        textAlign: 'center',
+                                        ...FONTS.body3,
+                                        color: COLORS.grey
+                                    }}
+                                >
+                                    {item.sub_title}
+                                </Text>
+                            </View>
                         </View>
                     )
                 }}
 
             />
+
+            {/* Footer */}
+            {renderFooter()}
         </View>
     )
 }
